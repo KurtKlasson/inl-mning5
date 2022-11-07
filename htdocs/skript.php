000b405952d706daa9a850240d0f828fe5269794
Calle <derpninjas88rgmail.com>
2022-11-07 10:42:07 +0100

    first commit

---
+++ htdocs/skript.php
@@ -0,0 +1,27 @@
+<?php
+ 
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "webbserverprogrammering";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+$login_success = false;
+$full_name = "";
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+		if($row["username"] == $_POST["username"] &&
+					$row["password"] == $_POST["password"]) {
+			$login_success = true;
+            echo "Välkommen ", $_POST["username"];
+                    }
+	}
+} else {
+    echo "0 results";
+}
+$conn->close();
